f1a240bac9a70bd34c7f37bf17510148eef6f6bd
Shaden-mdn <75998139+Shaden-mdn@users.noreply.github.com>
2022-02-12 21:21:30 +0300

    Another edit

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+  baseURL: 'https://my-json-server.typicode.com/Shaden-mdn/real-vue-shd',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
